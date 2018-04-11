2724d6d81d716a94f4bc067522e75a0c2b2254f4
akoes <abhishekkansal97@gmail.com>
2018-04-11 19:07:55 +0530

    add nice to have msg
    Signed-off-by: akoes <abhishekkansal97@gmail.com>

--- file01.js
+++ file01.js
@@ -3,6 +3,7 @@ function greet(name){
 	console.log('knock knock knock '+ name)
 	console.log('knock knock knock '+ name)
 	console.log('Nice to have you here')
+	console.log('would you like some tea?')
 }
 
 greet('ak')
